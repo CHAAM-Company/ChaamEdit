5227dca4909077ee194193b4946195f7e15f9faa
jeohong <jeohong59@gmail.com>
2022-10-08 20:20:52 +0900

    [ADD] Project Init

---
+++ SABO/SABO/SABOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SABOApp.swift
+//  SABO
+//
+//  Created by Hong jeongmin on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct SABOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
