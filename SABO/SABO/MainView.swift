f6f4e7374a60cf762f2d5ccb92cbb3b8f7cdefc5
Wonhyuk Choi <whchoi000@gmail.com>
2022-10-09 16:53:55 +0900

    [Fix] 유저 로그인 추가

--- SABO/SABO/MainView.swift
+++ SABO/SABO/MainView.swift
@@ -6,25 +6,44 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct MainView: View {
+    let firebaseAuth = Auth.auth()
+    @State var isSignIn = false
+    @ObservedObject var userVM = UserViewModel()
+    
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image("house")
+        ZStack {
+            if isSignIn {
+                TabView {
+                    HomeView()
+                        .tabItem {
+                            Image("house")
+                        }
+                    Text("grid")
+                        .tabItem {
+                            Image("grid")
+                        }
+                    Text("person")
+                    
+                        .tabItem {
+                            Image(systemName: "person.circle.fill")
+                        }
                 }
-            Text("grid")
-                .tabItem {
-                    Image("grid")
-                }
-            Text("person")
-            
-                .tabItem {
-                    Image(systemName: "person.circle.fill")
-                }
+                .accentColor(Color("TabColor"))
+            } else {
+                LoginView()
+            }
         }
-        .accentColor(Color("TabColor"))
+        .onAppear {
+            if firebaseAuth.currentUser != nil {
+                isSignIn = true
+                // userVM.getUserData(uid: firebaseAuth.currentUser!.uid)
+            } else {
+                isSignIn = false
+            }
+        }
     }
 }
 
