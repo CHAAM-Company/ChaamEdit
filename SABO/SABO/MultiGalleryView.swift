66f8fcca166abfa4b9502cac54b8646920255ae8
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-09 11:34:39 +0900

    [Feat] SwiftUI 파일로 MultiGalleryView 파일 재생성

---
+++ SABO/SABO/MultiGalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  MultiGalleryView.swift
+//  SABO
+//
+//  Created by 이재웅 on 2022/10/09.
+//
+
+import SwiftUI
+
+struct MultiGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultiGalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultiGalleryView()
+    }
+}
--- SABO/SABO/MultiGallyerView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MultiGallyerView.swift
-//  SABO
-//
-//  Created by 이재웅 on 2022/10/09.
-//
-
-import Foundation
