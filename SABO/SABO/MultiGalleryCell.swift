6547d958d64b5220fede8ba360307f2e4cf9b33b
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-09 12:10:28 +0900

    [Feat] DummyGalleryImage 에셋 추가

---
+++ SABO/SABO/MultiGalleryCell.swift
@@ -0,0 +1,20 @@
+//
+//  MultiGalleryCell.swift
+//  SABO
+//
+//  Created by 이재웅 on 2022/10/09.
+//
+
+import SwiftUI
+
+struct MultiGalleryCell: View {
+    var body: some View {
+        Rectangle()
+    }
+}
+
+struct MultiGalleryCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MultiGalleryCell()
+    }
+}
