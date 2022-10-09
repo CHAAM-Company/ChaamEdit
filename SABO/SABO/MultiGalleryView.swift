bc951ca5147d6c94b22802692d8c804fb3c0d6a0
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-10-09 12:07:35 +0900

    [Feat] MultiGalleryView에 LazyGrid, 타이틀텍스트 구현

--- SABO/SABO/MultiGalleryView.swift
+++ SABO/SABO/MultiGalleryView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct MultiGalleryView: View {
+    // Test Data
+    let data = Array(1...8).map { "목록 \($0)"}
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 160)),
+        GridItem(.adaptive(minimum: 160))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("보면 볼수록 늘어나는 편집 지식")
+                        .multilineTextAlignment(.leading)
+                        .font(.system(size: 32, weight: .bold))
+                        .frame(height: 80)
+                        .allowsTightening(false)
+                        .padding(.horizontal)
+                    
+                    LazyVGrid(columns: columns) {
+                        ForEach(data, id: \.self) { i in
+                            ZStack {
+                                //TODO: MultiGalleryRow로 구현 후 변경필요
+                                Rectangle()
+                                    .fill(Color.yellow)
+                                    .frame(height: 170.0)
+                                VStack {
+                                    Spacer()
+                                    Text(i)
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
